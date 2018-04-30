86a314590c8527786617d43276c7cbd9f2d22f50
adbir <36879599+adbir@users.noreply.github.com>
2018-04-30 08:52:24 +0200

    Tilføjet en int
    Der returneres et number

--- GitHub4Begyndere.java
+++ GitHub4Begyndere.java
@@ -2,4 +2,9 @@ public interface GitHub4Begyndere
 {
       public abstract void getGitHub();
       public abstract void getAdamErikLasseEmilie();
+
+      public int getInt()
+      {
+            return number;
+      }
 }
