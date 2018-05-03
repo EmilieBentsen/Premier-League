6fb0f9ee21d6e9e26e5b54dcbe13958b67cc5131
EmilieBentsen <33421005+EmilieBentsen@users.noreply.github.com>
2018-05-03 14:36:56 +0200

    Tilføjet metode GoalsByFootballer i GoalHandler
    Metode til at lave en optælling af en footballers samlede mål i sæsonen. Returnerer resultatet.

--- handlers/GoalHandler.java
+++ handlers/GoalHandler.java
@@ -2,5 +2,16 @@ package handlers;
 
 public class GoalHandler
 {
-
+      public int GoalsByFootballer(ArrayList<Goal> list, int footballerID) //metoden tæller alle mål med matchende footballer id i arrayet og ligger dem sammen.
+      {
+            int count;
+            for(goal i : list)
+            {
+                   if (i.getID() == id)
+                  {
+                        count++;
+                  }
+            }
+            return count;
+      }
 }
