7373ab9a930f5fb6cfb951921224b2b1ad5f4705
EmilieBentsen <33421005+EmilieBentsen@users.noreply.github.com>
2018-05-03 11:26:51 +0200

    tilføjet den rigtige FileHandler

--- handlers/FileHandler.java
+++ handlers/FileHandler.java
@@ -1,47 +1,50 @@
 package handlers;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
-public class FileHandler
-{     
-      public static final String FOOTBALLER_TXT = "footballer.txt"; // Skal disse 4 filer ikke ligge i deres handlers?
-      public static final String GOAL_TXT = "goal.txt";
-      public static final String OPPONENT_TXT = "opponent.txt";
-      public static final String MATCH_TXT = "match.txt";
-
-      public static String getFileContent(String path)
+class FileHandler
+{
+      public static <E extends Object> void save(String path, ArrayList<E> list, 
+            PersistentObjectHandler<E> handler)  //E kan være enhver type af objekter som extender Object klassen
+                                                //metoden returnerer ikke noget. Den tager en String og en Arraylist med objekter E som parameter.
       {
-            String content = "";
+            try
+            {
+                  boolean firstElement = true;
+                  PrintStream output = new PrintStream(new File(path)); 
+                  for(E element : list)
+                  {
+                        if(!firstElement) output.print('\n');
+                        else firstElement = false;
+                        output.print(handler.lineFrom(element));
+                  }
+            }
+            catch(FileNotFoundException e)
+            {
             
+            }
+                
+      }
+      
+      public static <E extends Object> ArrayList<E> getContent(String path, 
+            PersistentObjectHandler<E> handler) //metoden bruger en stirng og klassen PersistentObjectHandler<E> handler, som parameteter og returnerer en arraylist
+      {
+            ArrayList<E> list = new ArrayList<E>();
             try
             {
                   Scanner sc = new Scanner(new File(path));
-                  
-                  content += sc.nextLine();
-                  
                   while(sc.hasNextLine())
                   {
-                        content += '\n' + sc.nextLine();
+                        E object = handler.objectFrom(sc.nextLine());
+                        list.add(object);
                   }
             }
-            catch (FileNotFoundException e)
+            catch(FileNotFoundException e)
             {
-                  System.out.println("File not found.");
-            }
             
-            return content;
-      }
-      
-      public static void writeFileContent(String path, String content)
-      {     
-            try
-            {
-                  PrintStream output = new PrintStream(new File(path));
-                  output.print(content); 
             }
-            catch (FileNotFoundException e)
-            {
-                  System.out.println("Cannot access file: " + path);
-            }
+            return list;
       }
 }
