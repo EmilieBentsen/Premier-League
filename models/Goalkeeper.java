95d030f60ca8fdbe5329a477d9d51877ed81b06d
Lassemis <38852165+Lassemis@users.noreply.github.com>
2018-05-04 11:30:13 +0200

    Fejlrettelser i goalkeeper

--- models/Goalkeeper.java
+++ models/Goalkeeper.java
@@ -3,14 +3,14 @@ package models;
 
 public class Goalkeeper extends Footballer 
 {
-      public Goalkeeper(int footballerJersey, String footballerName, String footballerPosition, String footballerSalary, boolean footballerEmployed)
+      public Goalkeeper(int footballerJersey, String footballerName, String footballerSalary, boolean footballerEmployed)
       {
-            super(footballerJersey,footballerName,footballerPosition,footballerSalary,footballerEmployed);
+            super(footballerJersey,footballerName,footballerSalary,footballerEmployed);
       }
       
       public Goalkeeper(int id, int footballerJersey, String footballerName, String footballerPosition, String footballerSalary, boolean footballerEmployed)
       {
-            super(id,footballerJersey,footballerName,footballerPosition,footballerSalary,footballerEmployed);
+            super(id,footballerJersey,footballerName,footballerSalary,footballerEmployed);
       }
       
       public String getFootballerPosition()
