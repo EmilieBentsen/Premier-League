e4226f6de973d8efd44c7c41f5941a5a2c140dce
Lassemis <38852165+Lassemis@users.noreply.github.com>
2018-05-07 11:34:28 +0200

    Menuer er blevet oprettet
    består udelukkende af pseudokode.

---
+++ menus/StatisticsMenu.java
@@ -0,0 +1,26 @@
+package menus;
+
+public class StatisticsMenu
+{
+      public void statisticsMenu()
+      {
+      /* Printer menu header (You'll never walk alone" box + Menu navn/overskift).
+      Printer menuoversigt("1. Top 3 scorer, 2. FootBaller, 3. Club, 4. Match)
+      Kører input metode der printer prompts for input.
+      Kør metoder i baseret på input.
+      Hvis input = 1 : kør input metode til at få en periode, kald top3scorer() med input som parameter.
+      Hvis input = 2 : Printer liste af fodboldspilerer, kører input metode om at få et JerseynNumber,
+      og prnter spiller oplysinger fro den valgte spiller (visningaf spiller information kommer til at 
+      blive en "spændende" metode).
+      Hvis input = 3 : ???(hvilken statistik skal man kunne få om klubben? skal det være for en given periode?
+      Skal den altid vise den samme skærm?).
+      Hvis input = 4: (evt. prompt bruger til at indtaste periode) Print liste over kampe brugeren kan vælge 
+      mellem, kør input metode der beder om at få et match id for en af de viste matches.
+      Kald metode der tager imod en Match/MatchID, og viser detaljerede information baseret om pågældende match*/
+      }
+      
+      
+      
+      
+      
+}
