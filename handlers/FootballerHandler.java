f18db8f9d190caa6e00f1718b087efbe305f8574
adbir <36879599+adbir@users.noreply.github.com>
2018-05-09 11:44:15 +0200

    GoalHandler topGoalscorer!
    FUCKING HELL MATE

--- handlers/FootballerHandler.java
+++ handlers/FootballerHandler.java
@@ -128,7 +128,11 @@ public class FootballerHandler extends ObjectHandler<Footballer> //specifocerer 
              case "MN":
                         footballers.add(new Manager(getNewFootballerID(), footballerJersey, fooballerName, footballerSalary, footballerEmployed));
                         save(footballers);
-             }
-             
+             }      
+      }
+      
+      public Integer[][] getTop3Goals(ArrayList goalScorers)
+      {
+          goalScorers
       }
 }
